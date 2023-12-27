17ddfde6ace134aa9a3e3a8dc9ff75b307fcedbc
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-27 21:30:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const quoteElement = document.getElementById('dynamic-quote');
-    if (quoteElement) {
+    if (quoteElement && window.quotes && window.quotes.length > 0) {
         const randomIndex = Math.floor(Math.random() * window.quotes.length);
         quoteElement.textContent = window.quotes[randomIndex];
     }
