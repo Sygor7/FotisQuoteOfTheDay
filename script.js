2853afcd87c139a4668d428b0e9d0af25accfa83
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-27 21:10:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// You can add JavaScript functionality here if needed
-console.log('Script loaded.');
+const quotes = require('./quotes');
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const quoteElement = document.getElementById('dynamic-quote');
+    if (quoteElement) {
+        quoteElement.textContent = getRandomQuote();
+    }
+});
