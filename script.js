cc8dbe32c4290ebd8754ccd11305402686863bea
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-27 21:22:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const quotes = require('./quotes');
-
-function getRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     const quoteElement = document.getElementById('dynamic-quote');
     if (quoteElement) {
-        quoteElement.textContent = getRandomQuote();
+        const randomIndex = Math.floor(Math.random() * window.quotes.length);
+        quoteElement.textContent = window.quotes[randomIndex];
     }
 });
