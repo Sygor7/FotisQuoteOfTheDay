840efb2bbff398878359fc11f0879324109d3d23
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-28 00:58:34 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,6 +1,16 @@
 const quotes = [
     "É comu a vaianedda, avi bisognu du palu.",
-    "Fante, Cavallo e Re" //FIXME remove
+    "Viene una morte ogni volta di Papa",
+    "Lo sai che le Tachipirina se le dividi diventan cin... due da cinquecento",
+    "Problemi, problemi, problemi, problemi per Nino Foti",
+    "It's you year!",
+    "U pe mia fra staiu morendu, aaaaaaaaah!",
+    "Ho una voglia di prenderti a pugni cantando l'inno della Juve",
+    "Siuuuuuuuuuum!",
+    "Kiko è una spalla su",
+    "Ho preso un pesce",
+    "La matriciana più buona che tu abbia mai mangiato",
+    "Ho il cazzo da nero"
 ];
 
 // Export the array
