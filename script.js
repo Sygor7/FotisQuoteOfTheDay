766bbf760c20df385ddf0867404a4c11c59f7fc4
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-27 22:09:10 +0100

    update quotes styles

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const randomImageIndex = Math.floor(Math.random() * window.images.length);
 
         quoteElement.textContent = window.quotes[randomQuoteIndex];
-        quoteElement.innerHTML = '<i>' + window.quotes[randomQuoteIndex] + '</i>';
+        quoteElement.innerHTML = '<em>' + window.quotes[randomQuoteIndex] + '</em>';
         imageElement.src = 'images/' + window.images[randomImageIndex];
         imageElement.alt = 'Random Image';
     }
