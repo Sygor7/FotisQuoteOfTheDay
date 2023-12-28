70fb9f55d0aef7856b63240ec2b5043b9ec076a1
Sygor7 <54560627+Sygor7@users.noreply.github.com>
2023-12-28 12:59:57 +0100

    Update images.js

--- images.js
+++ images.js
@@ -1,7 +1,20 @@
 // images.js
 const images = [
-    'image1.jpg',
-    'image2.jpg'
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+    '10.jpg'
+    '11.jpg',
+    '12.jpg',
+    '13.jpg',
+    '14.jpg',
+    '15.jpg'
 ];
 
 // Export the array
